7adb27ac8da3d0cdecbbe5d626c409c797864e7a
Noreth29 <noreth.guajardo@iteso.mx>
2025-02-18 09:38:27 -0600

    prueba 1

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -1,15 +1,32 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Comienza con A
 """
 
 # Declaraciones
-CONSTANTE = valor
+#Pedir entrada
+#Si comienza con A
+#Si comienza con a
+#Si comienza con Á
+#Si comienza con á
 
 # Entradas
-entrada = input()
+entrada = input("Escribe una palabra: ")
 
 # Proceso
+if ord(entrada [0]) == ord("A"):
+    resultado = str(entrada) + " comineza con 'A'"
 
+elif ord(entrada [0]) == ord("Á"):
+    resultado = str(entrada) + " comineza con 'A'"
+
+elif ord(entrada [0]) == ord("a"):
+    resultado = str(entrada) + " comineza con 'A'"
+
+elif ord(entrada [0]) == ord("á"):
+    resultado = str(entrada) + " comineza con 'A'"
+
+else:
+    resultado = str(entrada) + " no comienza con 'A' "
 
 # Salidas
-print(salida)
+print(resultado)
